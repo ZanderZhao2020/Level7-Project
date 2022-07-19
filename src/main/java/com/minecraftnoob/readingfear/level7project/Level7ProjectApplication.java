d131f4580dd33b9a925d54119bd051c475cc6d39
League Student <zanderzhao2020@gmail.com>
2022-07-18 17:04:04 -0700

    Initial commit

---
+++ src/main/java/com/minecraftnoob/readingfear/level7project/Level7ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.minecraftnoob.readingfear.level7project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level7ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Level7ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minecraftnoob/readingfear/level7project/Level7ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minecraftnoob.readingfear.level7project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level7ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
